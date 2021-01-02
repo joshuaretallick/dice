579bac60a53f24fb9d98158f6d227b5337c8f828
Joshua Retallick <JoshOldEmpire@MacBook-Pro-3.local>
2021-01-02 17:55:19 +0000

    Created events table with all elements according to brief

---
+++ db/migrate/20210102175117_create_events.rb
@@ -0,0 +1,15 @@
+class CreateEvents < ActiveRecord::Migration[6.1]
+  def change
+    create_table :events do |t|
+      t.string :name
+      t.string :venue
+      t.float :price
+      t.string :category
+      t.integer :stock
+      t.date :date
+      t.time :time
+
+      t.timestamps
+    end
+  end
+end
